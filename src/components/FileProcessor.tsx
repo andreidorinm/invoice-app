74ce21175f87786bba52dbfdcf6fec606b4e938f
andreidorinm <andrei.dorin09@yahoo.com>
2024-03-03 23:03:14 +0200

    made a change

--- src/components/FileProcessor.tsx
+++ src/components/FileProcessor.tsx
@@ -214,7 +214,7 @@ const FileProcessor = () => {
             <div className="flex justify-center py-2">
               <button
                 onClick={openDialog}
-                className="px-10 py-4 bg-green-500 text-white rounded-lg hover:bg-green-600 focus:outline-none transition duration-150 ease-in-out"
+                className="px-10 py-4 bg-red-500 text-white rounded-lg hover:bg-red-600 focus:outline-none transition duration-150 ease-in-out"
               >
                 Selectează fișierul XML ANAF
               </button>
